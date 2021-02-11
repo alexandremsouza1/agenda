2572d6fdcf41dd77f7742b4ab55cee9494048cd0
Alexandre Magno <pedestaljunior@gmail.com>
2021-02-10 23:18:35 -0300

    file axios

---
+++ src/boot/axios.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+// we add it to Vue prototype
+// so we can reference it in Vue files as this.$axios
+// without the need to import axios or use vue-axios
+Vue.prototype.$axios = axios
+
+// can also create an axios instance specifically for the backend API
+const api = axios.create({ baseURL: 'http://127.0.0.1:3333/api' })
+Vue.prototype.$api = api
+
+export { axios, api }
